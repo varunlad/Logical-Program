c8ca5cc8109af195cdd78ff669de04bb266562a1
varun <varunlad5@gmail.com>
2021-09-08 11:33:41 +0530

    [varun] Logical Programs Fibonacci series

---
+++ LogicalPrograms/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1=0 , n2=1 , n3, n;
+            Console.WriteLine("Enter a number up to which you want the series : ");
+            n= Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                Console.WriteLine(n1);
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
